b019a078b7a240b8f1489afcd9589baa44a6546b
Rahhul7240 <001kingrahul@gmail.com>
2022-10-28 12:36:00 +0530

    nothing change

--- models/all-coins.js
+++ models/all-coins.js
@@ -5,30 +5,41 @@ let allCoins = new mongoose.Schema({
     S_No: {
         type: Number,
     },
+    Id :{
+        type:String,
+        unique :true
+    },
     coinId: {
         type: Number,
+        unique:true,
+        default:null
     },
     coinName: {
         type: String,
+        unique:true,
+        default:null
     },
     symbol: {
         type: String,
+        unique:true,
+        default:null
     },
     protocol: {
         type: String,
     },
     network: {
         type: String,
+        
     },
     website: {
-        type: String,
+        type: [String],
     },
     explore_Link: {
-        type: String,
+        type: [String],
     },
 
     socialMedia: {
-        type: String,
+        type: [String],
     }
 });
 
